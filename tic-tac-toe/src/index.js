7f15ef243a135c72a47493958f6e42c4a285c1a5
Jason Wee <peichieh@gmail.com>
2021-12-24 10:03:44 +0800

    progress

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+// this is function component
+function Square(props) {
+  return (
+     <button className="square" onClick={props.onClick}>
+        {props.value}
+     </button>
+  );
 
+}
+
+/*
 class Square extends React.Component {
 
   render() {
@@ -17,6 +27,7 @@ class Square extends React.Component {
     );
   }
 }
+*/
 
 // this is component
 class Board extends React.Component {
@@ -25,13 +36,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
        squares: Array(9).fill(null),
+       xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext? 'X' : 'O';
+    this.setState({
+	squares: squares,
+        xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -43,7 +58,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
