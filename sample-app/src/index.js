5fa8b1f5711fb2c577d57bd47f7ae5f049b3c003
Jason Wee <peichieh@gmail.com>
2021-12-27 23:00:43 +0800

    load index.js back

--- sample-app/src/index.js
+++ sample-app/src/index.js
@@ -1,18 +1,4 @@
-import React, { Component } from "react";
-import {hot} from "react-hot-loader";
-//import ReactDOM from "react-dom";
-//import App from "./App.js";
-import "./App.css";
-//ReactDOM.render(<App />, document.getElementById("root"));
-//
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, World! </h1>
-      </div>
-    );
-  }
-}
-
-export default hot(module)(App);
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+ReactDOM.render(<App />, document.getElementById("root"));
