d1fa6e4363d6c01eedd6748ba53bdd5d96beb18e
Jason Wee <peichieh@gmail.com>
2021-12-29 21:34:07 +0800

    react router

--- w3schools/src/index.js
+++ w3schools/src/index.js
@@ -1,6 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import Car from './Car.js';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+// import Car from './Car.js';
 
 /*
 const myfirstelement = <h1>Hello React!</h1>
@@ -10,4 +15,21 @@ ReactDOM.render(
   document.getElementById('root')
 );
 */
-ReactDOM.render(<Car/>, document.getElementById('root'));
+//ReactDOM.render(<Car/>, document.getElementById('root'));
+
+export default function App() {
+   return (
+      <BrowserRouter>  
+         <Routes>
+	    <Route path="/" element={<Layout/>}>
+	       <Route index element={<Home/>}/>
+	       <Route path="blogs" element={<Blogs/>}/>
+	       <Route path="contact" element={<Contact/>}/>
+	       <Route path="*" element={<NoPage/>}/>
+	    </Route>
+         </Routes>
+      </BrowserRouter>
+   );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
