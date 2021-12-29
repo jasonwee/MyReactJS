f08a5170ff72a72bbfb6fd4024e4eb652961f089
Jason Wee <peichieh@gmail.com>
2021-12-29 19:44:38 +0800

    import car function component

--- w3schools/src/index.js
+++ w3schools/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Car from './Car.js';
 
+/*
 const myfirstelement = <h1>Hello React!</h1>
 
 ReactDOM.render(
   myfirstelement,
   document.getElementById('root')
 );
+*/
+ReactDOM.render(<Car/>, document.getElementById('root'));
