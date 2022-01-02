3f3ab0c1782e6f5da30aabe893e7f51bc7f6d58e
Jason Wee <peichieh@gmail.com>
2022-01-02 13:58:52 +0800

    learn containment

--- tutorialpoint/expense-manager-reactjs/src/components/ExpenseEntryItem.js
+++ tutorialpoint/expense-manager-reactjs/src/components/ExpenseEntryItem.js
@@ -13,13 +13,18 @@ class ExpenseEntryItem extends React.Component {
    }
 
    render() { 
+      console.log("calling render1");
       return (
          //<div className="itemStyle">
+         <div> 
+         <div>{this.props.header}</div>     
          <div className={styles.itemStyle}>
             <div><b>Item:</b> <em>{this.props.name}</em></div>
             <div><b>Amount:</b> <em><FormattedMoney value={this.props.amount} /></em></div>
             <div><b>Spend Date:</b> <em><FormattedDate value={this.props.spendDate} /></em></div>
             <div><b>Category:</b> <em>{this.props.category}</em></div>
+         </div>
+         <div>{this.props.footer}</div> 
          </div>
       );
    }
