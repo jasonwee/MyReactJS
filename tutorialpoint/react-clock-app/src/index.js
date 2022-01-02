6d2f0160fac7c7d75643f6d1d65f05d71ff698e4
Jason Wee <peichieh@gmail.com>
2022-01-02 14:00:10 +0800

    react clock app

---
+++ tutorialpoint/react-clock-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Clock from './components/Clock';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Clock />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
