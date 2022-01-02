97c1519e3e6d91f28b305654b375fd29577298d3
Jason Wee <peichieh@gmail.com>
2022-01-02 13:59:14 +0800

    react lifecycle.

--- tutorialpoint/expense-manager-reactjs/src/components/ExpenseEntryItemList.js
+++ tutorialpoint/expense-manager-reactjs/src/components/ExpenseEntryItemList.js
@@ -4,11 +4,39 @@ import './ExpenseEntryItemList.css';
 class ExpenseEntryItemList extends React.Component {
    constructor(props) {
       super(props);
-
+      this.state = {
+         items: this.props.items
+      }
       this.handleMouseEnter = this.handleMouseEnter.bind();
       this.handleMouseLeave = this.handleMouseLeave.bind();
       this.handleMouseOver = this.handleMouseOver.bind();
    }
+
+   componentDidMount() {
+      console.log("ExpenseEntryItemList :: Initialize :: componentDidMount :: Component mounted");
+   }
+   shouldComponentUpdate(nextProps, nextState) {
+      console.log("ExpenseEntryItemList :: Update :: shouldComponentUpdate invoked :: Before update");
+      return true;
+   }
+   static getDerivedStateFromProps(props, state) {
+      console.log("ExpenseEntryItemList :: Initialize / Update :: getDerivedStateFromProps :: Before update");
+      let newState = {
+         items: props.items
+      }
+      return newState;
+   }
+   getSnapshotBeforeUpdate(prevProps, prevState) {
+      console.log("ExpenseEntryItemList :: Update :: getSnapshotBeforeUpdate :: Before update");
+      return null;
+   }
+   componentDidUpdate(prevProps, prevState, snapshot) {
+      console.log("ExpenseEntryItemList :: Update :: componentDidUpdate :: Component updated");
+   }
+   componentWillUnmount() {
+      console.log("ExpenseEntryItemList :: Remove :: componentWillUnmount :: Component unmounted");
+   }
+
    handleMouseEnter(e) {
       e.target.parentNode.classList.add("highlight");
    }
@@ -18,13 +46,44 @@ class ExpenseEntryItemList extends React.Component {
    handleMouseOver(e) {
       console.log("The mouse is at (" + e.clientX + ", " + e.clientY + ")");
    }
+
+   handleDelete = (id, e) => {
+      e.preventDefault();
+      console.log(id);
+
+      if(this.props.onDelete != null)this.props.onDelete(id, e);
+      /*
+      this.setState((state, props) => {
+         let items = [];
+
+         state.items.forEach((item, idx) => {
+            if(item.id != id)
+               items.push(item)
+         })
+         let newState = {
+            items: items
+         }
+         return newState;
+      })
+      */
+   }
+
+   getTotal() {
+      let total = 0;
+      for(var i = 0; i < this.state.items.length; i++) {
+         total += this.state.items[i].amount
+      }
+      return total;
+   }
+
    render() {
-      const lists = this.props.items.map((item) =>
+      const lists = this.state.items.map((item) =>
          <tr key={item.id} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
             <td>{item.name}</td>
             <td>{item.amount}</td>
             <td>{new Date(item.spendDate).toDateString()}</td>
             <td>{item.category}</td>
+	    <td><a href="#" onClick={(e) => this.handleDelete(item.id, e)}>Remove</a></td>
          </tr>
       );
       return (
@@ -35,10 +94,17 @@ class ExpenseEntryItemList extends React.Component {
                   <th>Amount</th>
                   <th>Date</th>
                   <th>Category</th>
+	          <th>Remove</th>
                </tr>
             </thead>
             <tbody>
                {lists}
+               <tr>
+                  <td colSpan="1" style={{ textAlign: "right" }}>Total Amount</td>
+                  <td colSpan="4" style={{ textAlign: "left" }}>
+                     {this.getTotal()}
+                  </td> 
+               </tr>
             </tbody>
          </table>
       );
