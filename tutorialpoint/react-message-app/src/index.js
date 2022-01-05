c0283a0cb113547943ec9f85c71bdb63313d931c
Jason Wee <peichieh@gmail.com>
2022-01-05 18:23:39 +0800

    redux example

---
+++ tutorialpoint/react-message-app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import App from './components/App';
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <React.StrictMode>
+   <Provider store={store}>
+      <App />
+   </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
