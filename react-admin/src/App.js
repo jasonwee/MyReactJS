37a6d0ee1c391f099478ae72e12c6329afa59334
Jason Wee <peichieh@gmail.com>
2022-01-08 11:13:06 +0800

    added remaining routes

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -3,14 +3,28 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home';
+import OnBoarding from "./pages/OnBoarding";
+//import BusinessCustomers from "./pages/BusinessCustomers";
+//import ProductCatalog from "./pages/ProductCatalog";
+import JourneyDesigner from "./pages/JourneyDesigner";
+import Settings from './pages/Settings';
+import ApiSettings from "./pages/ApiSettings";
+import InvoiceTemplate from "./pages/InvoiceTemplate";
+//import Invoices from "./pages/Invoices";
+//import Support from './pages/Support';
 
 function App() {
   return (
      <Router>
         <Navbar/>
-	<Routes>
+        <Routes>
            <Route path='/' exact element={<Home/>}/>
-	</Routes>
+           <Route path='/onboarding' element={<OnBoarding/>} />
+           <Route path='/journey-designer' element={<JourneyDesigner/>} />
+           <Route path='/settings' element={<Settings/>} />
+           <Route path='/api-settings' element={<ApiSettings/>} />
+           <Route path='/invoice-template' element={<InvoiceTemplate/>} />
+        </Routes>
      </Router>
   );
 }
