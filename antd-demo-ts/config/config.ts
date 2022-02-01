f74c0e9cf5800986bfb5c3e5307524ff90e394ff
Jason Wee <peichieh@gmail.com>
2022-02-01 17:52:56 +0800

    added config

---
+++ antd-demo-ts/config/config.ts
@@ -0,0 +1,11 @@
+Import {defineConfig} from'umi';
+
+Export const config = defineConfig({
+  layout:{
+    //Support anything that does not require dom
+    // https://procomponents.ant.design/components/layout#prolayout
+    Name: "Ant Design",
+    Region: correct,
+    Layout: "side",
+  },
+});
