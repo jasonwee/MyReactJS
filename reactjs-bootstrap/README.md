fcb170374f368219e40de794747bb2ba9a05c8c3
Jason Wee <peichieh@gmail.com>
2022-03-31 19:18:56 +0800

    reactjs and bootstrap

---
+++ reactjs-bootstrap/README.md
@@ -0,0 +1,1 @@
+https://www.geeksforgeeks.org/how-to-use-bootstrap-with-react/
---
+++ reactjs-bootstrap/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello World
+        </p>
+        <button type="button" class="btn btn-primary">Primary</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
