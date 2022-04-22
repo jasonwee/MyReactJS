19af0cc222cdedf55a981a0e74816a21dba8277a
Jason Wee <peichieh@gmail.com>
2022-04-22 20:45:53 +0800

    do reactjs table

---
+++ myreactapp/README.md
@@ -0,0 +1,9 @@
+https://www.javatpoint.com/react-table
+
+does not work :(
+
+```
+source init-env-nodejs18.sh
+npx create-react-app reactjs-table
+npm install react-table  
+```
---
+++ myreactapp/src/App-b.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+
+import ReactTable from "react-table";
+
+const data = [{  
+        name: 'Ayaan',  
+        age: 26  
+        },{  
+        name: 'Ahana',  
+        age: 22  
+        },{  
+        name: 'Peter',  
+        age: 40   
+        },{  
+        name: 'Virat',  
+        age: 30  
+        },{  
+        name: 'Rohit',  
+        age: 32  
+        },{  
+        name: 'Dhoni',  
+        age: 37  
+        }]
+
+const columns = [{  
+       Header: 'Name',  
+       accessor: 'name'  
+      },{  
+      Header: 'Age',  
+      accessor: 'age'  
+      }]
+
+
+
+function App() {
+  return (
+     <div>  
+        <ReactTable  
+            data={data}  
+            columns={columns}  
+            defaultPageSize = {2}  
+            pageSizeOptions = {[2,4, 6]}  
+         />  
+     </div>        
+  );
+}
+
+export default App;
