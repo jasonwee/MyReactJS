abcc282a561b0cf289ba4f583df0a44a5a909170
Jason Wee <peichieh@gmail.com>
2022-05-08 15:21:43 +0800

    table vertical

---
+++ frontend/table_vertical/README.md
@@ -0,0 +1,7 @@
+https://mui.com/material-ui/react-table/
+
+
+```
+source init-env-nodejs18.sh
+npx create-react-app table_vertical
+```
---
+++ frontend/table_vertical/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import MyTable from './components/MyTable'
+
+function App() {
+  return (
+    <div className="App">
+      <MyTable/>
+    </div>
+  );
+}
+
+export default App;
