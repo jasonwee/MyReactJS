a3e6234d6891d4b27cb74a1e2831c723791a2206
Jason Wee <peichieh@gmail.com>
2022-10-07 20:21:25 +0800

    added sass and scss example

---
+++ sass/sass-example1/README.md
@@ -0,0 +1,8 @@
+https://www.mitrais.com/news-updates/reactjs-with-sass-scss/
+
+```
+$ source init-env-nodejs18.sh
+$ npx create-react-app sass-example1
+$ cd sass-example1
+$ npm install sass
+```
---
+++ sass/sass-example1/src/App.js
@@ -0,0 +1,18 @@
+//import './App.sass';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="purple">
+        <p>Purple</p>
+      </div>
+<div className="divider"></div>
+      <div className="black">
+        <p>Black</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
