32224e3aad8e71dafc2e4ec25b8c22c303fff9b4
Jason Wee <peichieh@gmail.com>
2023-06-28 20:41:01 +0800

    initial

---
+++ component-input-range/README.md
@@ -0,0 +1,7 @@
+```
+source init-env-nodejs17.sh
+npx create-react-app component-input-range --template typescript
+cd component-input-range
+echo "BROWSER=none" > .env
+npm start
+```
---
+++ component-input-range/src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import Button from './Button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button onClick={() => console.log("hi")}>
+          <span>click me</span>
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
