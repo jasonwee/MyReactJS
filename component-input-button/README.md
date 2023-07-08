d8a11398352de872969d689dfa21823c67a37609
Jason Wee <peichieh@gmail.com>
2023-07-08 17:39:53 +0800

    input-button

---
+++ component-input-button/README.md
@@ -0,0 +1,1 @@
+https://css-tricks.com/how-to-recreate-the-ripple-effect-of-material-design-buttons/
---
+++ component-input-button/babel.js
@@ -0,0 +1,29 @@
+const Button = ({ children }) => {
+  function createRipple(event) {
+    const button = event.currentTarget;
+
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+    circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+    circle.classList.add("ripple");
+
+    const ripple = button.getElementsByClassName("ripple")[0];
+
+    if (ripple) {
+      ripple.remove();
+    }
+
+    button.appendChild(circle);
+  }
+
+  return <button onClick={createRipple}>{children}</button>;
+};
+
+const App = () => {
+  return <Button>Click Me</Button>;
+};
+
