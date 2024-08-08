165a363c6dfa7d44073f2e0c8938c170f9860921
Jason Wee <peichieh@gmail.com>
2024-08-08 09:49:29 +0800

    pl2

---
+++ tanstack_query/src/PostsList2.js
@@ -0,0 +1,31 @@
+import {useQuery} from "@tanstack/react-query";
+import {getPosts} from "./api/posts";
+
+
+
+function PostsList2() {
+
+  const postsQuery = useQuery({
+      queryKey: ["posts"],
+      queryFn: getPosts,
+  })
+
+
+  if (postsQuery.status === "loading") return <h1>Loading...</h1> 
+  if (postsQuery.status === "error") {
+    return <h1>{JSON.stringify(postsQuery.error)}</h1> 
+  }
+
+  return (
+      <div>
+        <h1>Posts List 2</h1>
+        <ol>
+          {postsQuery.data.map(post => (
+            <div key={post.id}>{post.title}</div>
+           ))}
+        </ol>
+      </div>
+  )
+}
+
+export default PostsList2;
