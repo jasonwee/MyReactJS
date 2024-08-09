cc9b78d12753ba633c6b63f1590c6f39952fa52f
Jason Wee <peichieh@gmail.com>
2024-08-09 09:12:20 +0800

    api

---
+++ tanstack_query/api/posts.jsx
@@ -0,0 +1,18 @@
+
+
+
+
+export function getPost(id) {
+   return axios.get(``http://localhost:3000/posts/${id}).then(res => res.data)
+}
+
+export function createPost({title, body}) {
+   return axios
+      .post("http://localhost:3000/posts", {
+         title,
+         body,
+         userId: 1,
+         id: Date.now(),
+      })
+      .then(res => res.data)
+}
