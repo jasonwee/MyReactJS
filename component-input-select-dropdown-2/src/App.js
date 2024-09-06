38fb1c8abaf0351e28c23a3f16202d91a72481f4
Jason Wee <peichieh@gmail.com>
2024-09-07 07:45:05 +0800

    app.js

---
+++ component-input-select-dropdown-2/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import CustomSelect from './components/CustomSelect';
+
+function App() {
+ const [options, setOptions] = useState([
+    {
+      label: "Option 1",
+      value: "opt1",
+    },
+    {
+      label: "Option 2",
+      value: "opt2",
+    },
+    {
+      label: "Option 3",
+      value: "opt3",
+    },
+  ])
+
+  const handleChangeSelect = (e) => {
+    console.log(e)
+  }
+
+  return (
+    <div className="App">
+      <CustomSelect
+        options={options}
+        placeHolder='Please select...'
+        onChange={(e) => handleChangeSelect(e)}
+        isSearchable
+        isMulti
+      />
+    </div>
+  );
+}
+
+export default App;
