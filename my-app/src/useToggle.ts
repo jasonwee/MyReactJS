1d794ea879257f9c927d04e26deff93274c305b6
Jason Wee <peichieh@gmail.com>
2024-10-26 13:12:24 +0800

    feature(useToggle): custom hook

---
+++ my-app/src/useToggle.ts
@@ -0,0 +1,24 @@
+import { useState, useCallback } from "react";
+
+const useToggle = (
+  initialValue: boolean
+): [boolean, () => void, () => void, () => void] => {
+  const [show, setShow] = useState(initialValue);
+
+  const toggle = useCallback(() => {
+    setShow((prevState) => !prevState);
+  }, []);
+
+  const close = useCallback(() => {
+    setShow(false);
+  }, []);
+
+  const open = useCallback(() => {
+    setShow(true);
+  }, []);
+
+  return [show, toggle, close, open];
+};
+
+export default useToggle;
+
