84f9833d8155816e35c6b9615cb8a44f185a4857
Jason Wee <peichieh@gmail.com>
2025-04-04 00:44:24 +0800

    update

--- frontend/authentication/src/App.js
+++ frontend/authentication/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import "./App.css";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Login from "./components/Login/Login";
+import Preferences from "./components/Preferences/Preferences";
+import useToken from "./useToken";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   //const [token, setToken] = React.useState();
+   //const token = getToken();
+   const { token, setToken } = useToken();
+
+   if (!token) {
+      return <Login setToken={setToken} />;
+   }
+
+   return (
+      <div className="wrapper">
+         <h1>Application</h1>
+         <BrowserRouter>
+            <Routes>
+               <Route path="/dashboard" element={<Dashboard />} />
+               <Route path="/preferences" elmeent={<Preferences />} />
+            </Routes>
+         </BrowserRouter>
+      </div>
+   );
 }
 
 export default App;
