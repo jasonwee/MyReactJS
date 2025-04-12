f079fea7ed6211f44ea269ac353b6467634cde7e
Jason Wee <peichieh@gmail.com>
2025-04-13 00:04:50 +0800

    anotehr example

--- component-input-range/src/App.tsx
+++ component-input-range/src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {ChangeEvent, } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import DoubleRangeSlider from './DoubleRangeSlider';
+import DoubleRangeSlider1 from './DoubleRangeSlider1';
 
 function App() {
+
+  const toRenderDoubleRangeSlider : boolean = false;
+  const toRenderDoubleRangeSlider1 : boolean = true;
 
   const [state, setState] = React.useState({
     lowLevelValue: 5,
@@ -30,20 +34,69 @@ function App() {
       })
   }
 
+  const renderDoubleRangeSlider = () => {
+    return (
+      <DoubleRangeSlider
+      min={3}
+      max={20}
+      currentMax={state.highLevelValue}
+      currentMin={state.lowLevelValue}
+      setCurrentMinHandler={setCurrentMinHandler}
+      setCurrentMaxHandler={setCurrentMaxHandler}
+      defaultMin={5}
+      defaultMax={10}
+  />
+    )
+  }
+
+
+  const setMin = (e: ChangeEvent<HTMLInputElement>) => {
+    const { min, max, currentMax, minValueBetween } = this.state;
+    const inputMin = e.target.value;
+    
+    this.setState({
+        inputMin
+    });
+    
+    if((inputMin >= min) && (inputMin <= (currentMax-minValueBetween))){
+      this.setState({
+        currentMin: parseInt(inputMin)
+      }); 
+
+      this.minValue.style.width = (inputMin*100)/max + "%";
+    }
+  }
+
+  const setMax = (e: ChangeEvent<HTMLInputElement>) => {
+    const { min, max, currentMin, currentMax, minValueBetween } = this.state;
+
+    const inputMax = e.target.value;
+    
+    this.setState({
+        inputMax
+    });
+
+    if((inputMax >= currentMin + minValueBetween) && (inputMax <= max)){
+      
+      this.setState({
+        currentMax: parseInt(inputMax)
+      });
+      this.maxValue.style.width = (inputMax*100)/max + "%";
+    }
+      
+  }
+
+  const renderDoubleRangeSlider1 = () => {
+    return (
+      <DoubleRangeSlider1/>
+    )
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-          <DoubleRangeSlider
-              min={3}
-              max={20}
-              currentMax={state.highLevelValue}
-              currentMin={state.lowLevelValue}
-              setCurrentMinHandler={setCurrentMinHandler}
-              setCurrentMaxHandler={setCurrentMaxHandler}
-              defaultMin={5}
-              defaultMax={10}
-          />
+        {toRenderDoubleRangeSlider && renderDoubleRangeSlider()}
+        {toRenderDoubleRangeSlider1 && renderDoubleRangeSlider1()}
       </header>
     </div>
   );
