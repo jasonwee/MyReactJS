f3bda7bced0d29d8096b847ab7acec32e70198b3
Jason Wee <peichieh@gmail.com>
2025-08-06 18:33:41 +0800

    feature: make tooltip support direction where should the tooltip appear with respect to the element

--- component-tooltip/src/App.js
+++ component-tooltip/src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Tooltip } from "./components/Tooltip/Tooltip";
 const App = () => {
   return (
-     <>
+     <div style={{margin: "auto", width: "80%"}}>
 <h2>Tooltip</h2>
 <p>Move the mouse over the text below:</p>
 
     <Tooltip text="Hello there!" />
 
-     </>
+     </div>
  );
 };
 
--- component-tooltip/src/components/Tooltip/Tooltip.js
+++ component-tooltip/src/components/Tooltip/Tooltip.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./tooltip.css";
-export const Tooltip = ({ text }) => {
+
+export const Tooltip = ({ text, direction = "right" }) => {
    return (
-      <div className="tooltip">
+      <div className={`tooltip tooltip-${direction}`}>
          Hover over me 1
-         <span className="tooltiptext">{text}</span>
+         <span className={`tooltiptext tooltiptext-${direction}`}>{text}</span>
       </div>
    );
 };
